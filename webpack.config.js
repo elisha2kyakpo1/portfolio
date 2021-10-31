e1714ac988e157edbf573611bc5e3321710a10ac
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-10-31 15:13:20 +0200

    Add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'TodoList',
+      title: 'portfolio',
       template: './src/index.html',
     }),
   ],
